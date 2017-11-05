434c13a673867be1faf6ddae030fd9ec220caa09
dizsun <dizsun@126.com>
2017-11-05 23:20:00 +0800

    modified WxLoginServlet

--- src/servlet/RegisterServlet.java
+++ src/servlet/RegisterServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "RegisterServlet",urlPatterns = {"/register"})
+@WebServlet(name = "RegisterServlet", urlPatterns = {"/register"})
 public class RegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -19,15 +19,16 @@ public class RegisterServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RedisUtil redisUtil = new RedisUtil();
         String user_openid = redisUtil.queryString(request.getSession().getId());
-        String user_nickname=new String(request.getParameter("nickName").getBytes("ISO-8859-1"),"utf-8");
-        String user_avatarUrl=request.getParameter("avatarUrl");
-        if(user_openid!=null && !user_openid.equals("")){
-            DbDao dbDao = (DbDao)getServletContext().getAttribute("dbDao");
+//        String user_nickname=new String(request.getParameter("nickName").getBytes("ISO-8859-1"),"utf-8");
+        String user_nickname = request.getParameter("nickName");
+        String user_avatarUrl = request.getParameter("avatarUrl");
+        if (user_openid != null && !user_openid.equals("")) {
+            DbDao dbDao = (DbDao) getServletContext().getAttribute("dbDao");
             try {
-                if(dbDao.insert("insert into user_info(user_nickname,user_avatarUrl,user_openid) value(?,?,?)",user_nickname,user_avatarUrl,user_openid)){
+                if (dbDao.insert("insert into user_info(user_nickname,user_avatarUrl,user_openid) value(?,?,?)", user_nickname, user_avatarUrl, user_openid)) {
                     response.getWriter().write("200");
                     System.out.println("200");
-                }else {
+                } else {
                     response.getWriter().write("210");
                     System.out.println("210");
                 }
@@ -35,7 +36,7 @@ public class RegisterServlet extends HttpServlet {
                 response.getWriter().write("120");
                 System.out.println("120");
             }
-        }else {
+        } else {
             response.getWriter().write("220");
             System.out.println("220");
         }
