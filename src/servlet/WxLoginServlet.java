00497dd98a8ccc37ea44c31da122b21c7b7ec9e3
dizsun <dizsun@126.com>
2017-11-05 11:20:51 +0800

    modified WxLoginServlet

--- src/servlet/WxLoginServlet.java
+++ src/servlet/WxLoginServlet.java
@@ -22,13 +22,13 @@ public class WxLoginServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String appid = "wx2ba5834caba16490";
-
         String appsecret = "6dd9a0d6d2da385980b9f388400eeaa8";
         String js_code = request.getParameter("js_code");
         String requestUrl="https://api.weixin.qq.com/sns/jscode2session";
         String outputStr ="appid=wx2ba5834caba16490&secret=6dd9a0d6d2da385980b9f388400eeaa8&js_code=" + js_code + "&grant_type=authorization_code";
         String httpResult = ResponseJsonUtils.httpRequest(requestUrl,outputStr);
         DataBean data = ResponseJsonUtils.getData(httpResult);
+        request.getSession(true).setAttribute("flag",true);
 //        Map<String,Object> data=new HashMap<>();
 //        data.put("data",httpResult);
 //        ResponseJsonUtils.jsonp(response,data);
