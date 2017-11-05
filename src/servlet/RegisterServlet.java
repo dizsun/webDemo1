7a331ffd98879a17b8ce1d710887874542beb429
dizsun <dizsun@126.com>
2017-11-05 23:07:37 +0800

    modified WxLoginServlet

--- src/servlet/RegisterServlet.java
+++ src/servlet/RegisterServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Base64;
 
 @WebServlet(name = "RegisterServlet",urlPatterns = {"/register"})
 public class RegisterServlet extends HttpServlet {
@@ -39,6 +38,6 @@ public class RegisterServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doPost(request,response);
     }
 }
