a409ca54258261fbf1f1d103a58e0925e7a17a59
dizsun <dizsun@126.com>
2017-11-07 18:30:06 +0800

    修改了getAccount

--- src/servlet/addAccountServlet.java
+++ src/servlet/addAccountServlet.java
@@ -7,18 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "charsetServlet",urlPatterns = "/charset")
-public class charsetServlet extends HttpServlet {
+@WebServlet(name = "addAccountServlet")
+public class addAccountServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        request.setCharacterEncoding("ISO-8859-1");
-        response.setContentType("text/json;charset=UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write(request.getParameter("str"));
 
-        System.out.println(request.getParameter("str"));
     }
 }
